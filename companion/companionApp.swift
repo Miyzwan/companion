81a50a7792501f2bb3bcb775120cde4e2db6ef40
Dimas Dwi Ismaunnizam <dimasdwiisma@gmail.com>
2026-08-12 00:37:41 +0700

    Initial Commit

---
+++ companion/companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  companionApp.swift
+//  companion
+//
+//  Created by Dimas Dwi Ismaunnizam on 12/08/26.
+//
+
+import SwiftUI
+
+@main
+struct companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
